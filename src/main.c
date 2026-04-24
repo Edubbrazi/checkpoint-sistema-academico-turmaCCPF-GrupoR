80bf26dfb80765215745c0dc671668aba2ceb94c
lucasbomfim1207-ai <lucasbomfim1207@gmail.com>
2026-04-24 12:36:45 -0300

    Foi arrumado o loop de repetição do programa, foi adicionado parte estética no menu e correção das chaves

--- src/main.c
+++ src/main.c
@@ -1,41 +1,42 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void menu(){
-    printf("--- SISTEMA EQUIPE R ---\n");
+void menu() {
+    printf("==============================\n");
+    printf(" SISTEMA EQUIPE R\n");
+    printf("==============================\n");
     printf("1 - Inserir notas\n");
     printf("2 - Calcular media\n");
     printf("3 - Verificar situacao\n");
     printf("4 - Exibir resultado\n");
     printf("5 - Calcular derivada\n");
     printf("6 - Sair\n");
-    printf("Escolha uma opcao");
+    printf("Escolha uma opcao: ");
 }
 
-
 int main() {
+    int opcao;
+    float nota_1 = 0, nota_2 = 0, media = 0;
+    float a, b, c;
 
-int opcao, a, b, c;
-float nota_1, nota_2, media;
-menu();
-scanf("%d", &opcao);
-    
-switch(opcao){
-     case 1: 
-        printf("\n--- Insira as notas ---\n");
-        printf("Digite sua primeira nota: ");
-        scanf("%f", &nota_1);
-        printf("Digite sua segunda nota: ");
-        scanf("%f", &nota_2);
-        break; 
+    do {
+        menu();
+        scanf("%d", &opcao);
 
-    case 2:
-            media = (nota_1 + nota_2)/2;
-            printf("O aluno esta com a media [(%.2f + %.2f)/2] igual a %.2f.\n", nota_1, nota_2, media);
-        break;
-    
-            printf("O valor de frequencia invalida!");
-    
-     case 3:
+        switch(opcao) {
+            case 1:
+                printf("\n--- Insira as notas ---\n");
+                printf("Digite sua primeira nota: ");
+                scanf("%f", &nota_1);
+                printf("Digite sua segunda nota: ");
+                scanf("%f", &nota_2);
+                break;
+
+            case 2:
+                media = (nota_1 + nota_2) / 2;
+                printf("Media calculada: %.2f\n", media);
+                break;
+
+            case 3:
                 printf("\n--- Verificador de situacao ---\n");
                 if (media >= 6) {
                     printf("O aluno esta APROVADO.\n");
@@ -43,24 +44,37 @@ switch(opcao){
                     printf("O aluno esta REPROVADO.\n");
                 }
                 break;
-        
-    case 4:
-      printf("\n--- Exibcao de resultados ---\n");
-      printf("A Media do aluno é %f\n", media);
 
-    case 5:
-        printf("\n--- Calcular derivada ---\n");
-        printf("Digite o valor de A: ");
-        scanf("%f", &a);
-        printf("Digite o valor de B: ");
-        scanf("%f", &b);
-          printf("Digite o valor de C: ");
-        scanf("%f", &c);
+            case 4:
+                printf("\n--- Exibicao de resultados ---\n");
+                printf("Nota 1: %.2f\n", nota_1);
+                printf("Nota 2: %.2f\n", nota_2);
+                printf("Media: %.2f\n", media);
+                break;
 
-        
-        break;
+            case 5:
+                printf("\n--- Calculo da derivada ---\n");
+                printf("Funcao: f(x) = ax^2 + bx + c\n");
+                printf("Digite o valor de a: ");
+                scanf("%f", &a);
+                printf("Digite o valor de b: ");
+                scanf("%f", &b);
+                printf("Digite o valor de c (nao afeta a derivada): ");
+                scanf("%f", &c);
+
+                printf("f'(x) = %.2fx + %.2f\n", 2*a, b);
+                break;
+
+            case 6:
+                printf("Saindo do sistema...\n");
+                break;
+
+            default:
+                printf("Opcao invalida! Tente novamente.\n");
+                break;
+        }
+        printf("\n");
+    } while (opcao != 6);
 
     return 0;
-
-}
 }
