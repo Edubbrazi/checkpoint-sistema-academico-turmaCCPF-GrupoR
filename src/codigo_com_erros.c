67502d8269721276dc1f60be002fb56b617440f5
Julia Johanson <juliajohansonpeniche@gmail.com>
2026-04-24 11:04:57 -0300

    adicionada ; no final da linha 4, adicionada ; no final da linha 4, alterada condicao para o if

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -1,17 +1,17 @@
 #include <stdio.h> // foi substituidas as aspas pelo <>
 
 int main() {
-    float nota1, nota2, media
+    float nota1, nota2, media; //adicionada ; no final da linha 4
 
     printf("Digite a primeira nota: ");
-    scanf("%f", nota1);
+    scanf("%f", &nota1); // adicionado & para a variavel ser guardada na existente ao inves de criar outra
 
     printf("Digite a segunda nota: ");
     scanf("%f", &nota2);//
 
     media = nota1 + nota2 / 2;
 
-    if (media = 6) {
+    if (media = 6) { 
         printf("Aluno aprovado\n");
     } else {
         printf("Aluno reprovado\n");
