610dccd57ae2605a2e89c03bab08b58caa4a3eff
Julia Johanson <juliajohansonpeniche@gmail.com>
2026-04-24 10:57:17 -0300

    foi substituidas as aspas pelo <>

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -1,13 +1,13 @@
-#include "stdio.h"
+#include <stdio.h> // foi substituidas as aspas pelo <>
 
 int main() {
     float nota1, nota2, media
 
     printf("Digite a primeira nota: ");
-    scanf("%f", &nota1);
+    scanf("%f", nota1);
 
     printf("Digite a segunda nota: ");
-    scanf("%f", nota2);
+    scanf("%f", &nota2);//
 
     media = nota1 + nota2 / 2;
 
