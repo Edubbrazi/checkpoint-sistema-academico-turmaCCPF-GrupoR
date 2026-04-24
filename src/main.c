ac8a9fb41203c794156547bc448fce5a0c2f2f2c
edubbrazi@gmail.com <labsfiap@fiap.com.br>
2026-04-24 11:32:23 -0300

    Foi criado a função de menu e a variavel para receber as notas da media

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
 
+void menu(){
+    printf("--- SISTEMA EQUIPE R ---\n");
+    printf("1 - Inserir notas\n");
+    printf("2 - Calcular media\n");
+    printf("3 - Verificar situacao\n");
+    printf("4 - Exibir resultado\n");
+    printf("5 - Calcular derivada\n");
+    printf("6 - Sair\n");
+    printf("Escolha uma opcao");
+}
+
+
 int main() {
+
+int opcao;
+float nota_1, nota_2, media;
+menu();
+scanf("%d", &opcao);
     
+switch(opcao)
 
     
     return 0;
